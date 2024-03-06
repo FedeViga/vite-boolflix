f255aa40d95036e307a6cbfb9d88b83cfc356e57
federico_viganò <fede_viga@hotmail.it>
2024-03-06 11:46:43 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+import axios from 'axios';
+
+import {store} from './assets/js/store'
+
+export default {
+
+  data() {
+    return {
+      store,
+    }
+  },
+
+  created() {
+
+  },
+
+  methods: {
+
+  },
+
+  components: {
+
+  }
+}
+</script>
+
+
+<template>
+  CIA
+</template>
+
+<style lang="scss">
+
+@use './styles/general' as *;
+
+</style>
